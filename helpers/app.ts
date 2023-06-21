d5dab715e58765e037edb87cae042ec861216bff
PabloAT20 <pablotellez5@gmail.com>
2023-06-20 19:49:55 -0600

    tarea

--- helpers/app.ts
+++ helpers/app.ts
@@ -1,39 +1,28 @@
-// class TallerPC {
-
-//     servicios: string[];
-//     constructor() {
-//     this.servicios = [];
-//     }
-
-//     mostrarServicios(servicio: string) {
-//       console.log("Servicios ofrecidos:");
-//       for (servicio of this.servicios) {
-//         console.log(servicio);
-//       }
-//     }
-
-//     agregarServicio(servicio: string) {
-//       this.servicios.push(servicio);
-//       console.log(`Se ha agregado el servicio "${servicio}" al taller.`);
-//     }
-
-//     repararProducto(
-//       tipoProducto: string,
-//       nombreProducto: string,
-//       descripcionProblema: string
-//     ): void {
-//       console.log(`Reparando ${tipoProducto} "${nombreProducto}" con el siguiente problema: ${descripcionProblema}`);
-
-//       console.log(`¡${tipoProducto} "${nombreProducto}" reparado correctamente!`);
-//     }
-// }
-
-
-// const TallerPablo = new TallerPC ();
-// TallerPablo.agregarServicio("Reemplazo de disco duro");
-// TallerPablo.agregarServicio("Limpieza de ventiladores");
-// TallerPablo.agregarServicio("Instalación de software");
-// TallerPablo.mostrarServicios('servicios');
-// TallerPablo.repararProducto("Laptop", "ASUS", "mantenimineot general");
-
-
+//Crear una clase que se encargue de manejar los servicios de una cafeteria
+/**
+ *  
+ *  - Menu {
+ *      bebidas{
+ *         0: 'coca'
+ *      }
+ *      alimentos{
+ *          0: 'hot cakes'
+ *      }
+ *  }
+ * 
+ *  - Clientes {
+ *      idCliente new Date().getTime()
+ *      nombreCliente,
+ *      
+ *  }
+ * 
+ *  - Cafeteria {
+ *      idPedido
+ * 
+ *      mostrarMenu,
+ *      CancelarPedido,
+ *      levantarPedido,
+ *      MostrarPedido,
+ *  }
+ *  
+ */

b740d633cc3e865b8f05117b7a4f415b447394df
PabloAT20 <pablotellez5@gmail.com>
2023-06-21 22:16:51 -0600

    aqui

--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,27 @@
 "use strict";
-// class TallerPC {
-//     servicios: string[];
-//     constructor() {
-//     this.servicios = [];
-//     }
-//     mostrarServicios(servicio: string) {
-//       console.log("Servicios ofrecidos:");
-//       for (servicio of this.servicios) {
-//         console.log(servicio);
-//       }
-//     }
-//     agregarServicio(servicio: string) {
-//       this.servicios.push(servicio);
-//       console.log(`Se ha agregado el servicio "${servicio}" al taller.`);
-//     }
-//     repararProducto(
-//       tipoProducto: string,
-//       nombreProducto: string,
-//       descripcionProblema: string
-//     ): void {
-//       console.log(`Reparando ${tipoProducto} "${nombreProducto}" con el siguiente problema: ${descripcionProblema}`);
-//       console.log(`¡${tipoProducto} "${nombreProducto}" reparado correctamente!`);
-//     }
-// }
-// const TallerPablo = new TallerPC ();
-// TallerPablo.agregarServicio("Reemplazo de disco duro");
-// TallerPablo.agregarServicio("Limpieza de ventiladores");
-// TallerPablo.agregarServicio("Instalación de software");
-// TallerPablo.mostrarServicios('servicios');
-// TallerPablo.repararProducto("Laptop", "ASUS", "mantenimineot general");
+class cafeteria {
+    constructor() {
+        this.bebidas = ['coca', 'agua de limon', 'manzanita'];
+        this.alimentos = ['sándwich', 'ensalada', 'hot cakes', 'salchgipapas'];
+        //    this.idPedido = idPedido;
+        this.cliente = [];
+    }
+    EnseñaMenu() {
+        //bebidas
+        console.log('--- Menú ---\n');
+        console.log('Bebidas:');
+        this.bebidas.forEach((bebidas, index) => {
+            // return `${index}: ${bebida}`;
+            console.log(`${index}: ${bebidas}`);
+        });
+        //comida/alimentos
+        console.log('Alimentos:');
+        this.alimentos.forEach((alimento, index) => {
+            //   return `${index}: ${alimento}`;
+            console.log(`${index}: ${alimento}`);
+        });
+    }
+}
+const menuu = new cafeteria();
+menuu.EnseñaMenu();
 //# sourceMappingURL=app.js.map
--- helpers/app.ts
+++ helpers/app.ts
@@ -1,28 +1,37 @@
-//Crear una clase que se encargue de manejar los servicios de una cafeteria
-/**
- *  
- *  - Menu {
- *      bebidas{
- *         0: 'coca'
- *      }
- *      alimentos{
- *          0: 'hot cakes'
- *      }
- *  }
- * 
- *  - Clientes {
- *      idCliente new Date().getTime()
- *      nombreCliente,
- *      
- *  }
- * 
- *  - Cafeteria {
- *      idPedido
- * 
- *      mostrarMenu,
- *      CancelarPedido,
- *      levantarPedido,
- *      MostrarPedido,
- *  }
- *  
- */
+class cafeteria {
+    bebidas: string[];
+    alimentos: string[];
+    // idPedido: number;
+    cliente: string[];
+
+    constructor (){
+       this.bebidas = ['coca', 'agua de limon', 'manzanita'];
+       this.alimentos = ['sándwich','ensalada','hot cakes', 'salchgipapas'];
+    //    this.idPedido = idPedido;
+       this.cliente = [];
+    }
+
+    EnseñaMenu(){
+    //bebidas
+    console.log('--- Menú ---\n');
+    console.log('Bebidas:');
+    this.bebidas.forEach((bebidas, index) => {
+    // return `${index}: ${bebida}`;
+    console.log(`${index}: ${bebidas}`);
+    });
+    //comida/alimentos
+    console.log('Alimentos:');
+    this.alimentos.forEach((alimento, index) => {
+    //   return `${index}: ${alimento}`;
+    console.log(`${index}: ${alimento}`);
+    });
+    }
+
+    // cancelarPedido(idPedido:number) {
+    //     console.log(`Pedido ${idPedido} cancelado.`);
+    //   }
+
+}
+
+const menuu =  new cafeteria();
+menuu.EnseñaMenu();
